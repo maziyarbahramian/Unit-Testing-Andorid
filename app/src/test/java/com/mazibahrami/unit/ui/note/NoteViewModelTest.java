debd6888fafffdd95050844ca96c4a8dd1b55e92
Maziyar Bahramian <maziyar.bahramian@gmail.com>
2021-02-22 21:20:42 +0330

    test NoteViewModel

---
+++ app/src/test/java/com/mazibahrami/unit/ui/note/NoteViewModelTest.java
@@ -0,0 +1,127 @@
+package com.mazibahrami.unit.ui.note;
+
+import com.mazibahrami.unit.models.Note;
+import com.mazibahrami.unit.repository.NoteRepository;
+import com.mazibahrami.unit.ui.Resource;
+import com.mazibahrami.unit.util.InstantExecutorExtension;
+import com.mazibahrami.unit.util.LiveDataTestUtil;
+import com.mazibahrami.unit.util.TestUtil;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import io.reactivex.Flowable;
+import io.reactivex.internal.operators.single.SingleToFlowable;
+
+import static com.mazibahrami.unit.repository.NoteRepository.INSERT_SUCCESS;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(InstantExecutorExtension.class)
+public class NoteViewModelTest {
+
+    // system under test
+    private NoteViewModel noteViewModel;
+
+    @Mock
+    private NoteRepository noteRepository;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        noteViewModel = new NoteViewModel(noteRepository);
+    }
+
+    /*
+     * can't observe a note that hasn't been set
+     * */
+    @Test
+    void observeEmptyNoteWhenNoteSet() throws Exception {
+        // Arrange
+        LiveDataTestUtil<Note> liveDataTestUtil = new LiveDataTestUtil<>();
+
+        // Act
+        Note note = liveDataTestUtil.getValue(noteViewModel.observeNote());
+
+        // assert
+        assertNull(note);
+    }
+
+    /*
+     * observe a note has been set and onChanged will trigger in activity
+     * */
+    @Test
+    void observeNote_whenSet() throws Exception {
+        // Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+        LiveDataTestUtil<Note> liveDataTestUtil = new LiveDataTestUtil<>();
+
+        // Act
+        noteViewModel.setNote(note);
+        Note observedNote = liveDataTestUtil.getValue(noteViewModel.observeNote());
+
+        // assert
+        assertEquals(note, observedNote);
+    }
+
+    /*
+     * Insert a new note and observe row returned
+     * */
+    @Test
+    void insertNote_returnRow() throws Exception {
+        // Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+        LiveDataTestUtil<Resource<Integer>> liveDataTestUtil = new LiveDataTestUtil<>();
+        final int insertedRow = 1;
+        Flowable<Resource<Integer>> returnedData = SingleToFlowable.just(Resource.success(insertedRow, INSERT_SUCCESS));
+        when(noteRepository.insertNote(any(Note.class))).thenReturn(returnedData);
+
+        // Act
+        noteViewModel.setNote(note);
+        Resource<Integer> returnedValue = liveDataTestUtil.getValue(noteViewModel.insertNote());
+
+        // Assert
+        assertEquals(Resource.success(insertedRow, INSERT_SUCCESS), returnedValue);
+    }
+
+    /*
+     * Insert: don't return a new row without observer
+     * */
+    @Test
+    void dontReturnInsertRowWithoutObserver() throws Exception {
+        // Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+
+        // Act
+        noteViewModel.setNote(note);
+
+        // Assert
+        verify(noteRepository, never()).insertNote(any(Note.class));
+    }
+
+    /*
+     * set note, null title, throw exception
+     * */
+    @Test
+    void setNote_nullTitle_throwException() {
+        // Arrange
+        Note note = new Note(TestUtil.TEST_NOTE_1);
+        note.setTitle(null);
+
+        // Assert
+        assertThrows(Exception.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+
+                // Act
+                noteViewModel.setNote(note);
+            }
+        });
+    }
+}
